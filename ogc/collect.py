36e3f263035d5570116e7c21af300ea5f679ad7e
Adam Stokes <battlemidget@users.noreply.github.com>
2020-04-16 09:49:22 -0400

    tar up workdir
    Signed-off-by: Adam Stokes <battlemidget@users.noreply.github.com>

--- ogc/collect.py
+++ ogc/collect.py
@@ -60,7 +60,7 @@ class Collector:
     def artifacts(self):
         """ Tars up any artifacts in the job directory
         """
-        cmd_ok(f"tar -C {self.workdir} -cvzf artifacts.tar.gz *", shell=True)
+        cmd_ok(f"cd {self.workdir} && tar cvzf artifacts.tar.gz *", shell=True)
 
     def push(self, profile_name, region_name, bucket, db_key, files):
         """ Pushes files to s3, needs AWS configured prior
